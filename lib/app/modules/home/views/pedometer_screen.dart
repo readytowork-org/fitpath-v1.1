b8bb5ee89e9ee35254a474582c737a3961d261e9
pratik-adh <adhikaripratik014@gmail.com>
2022-06-10 18:04:12 +0545

    feat:Calculated distance using haversine formula

--- lib/app/modules/home/views/pedometer_screen.dart
+++ lib/app/modules/home/views/pedometer_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:pedometer/pedometer.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class PedometerScreen extends StatefulWidget {
+  const PedometerScreen({Key? key}) : super(key: key);
+
   @override
   _PedometerScreenState createState() => _PedometerScreenState();
 }
@@ -10,12 +13,34 @@ class PedometerScreen extends StatefulWidget {
 class _PedometerScreenState extends State<PedometerScreen> {
   late Stream<StepCount> _stepCountStream;
   late Stream<PedestrianStatus> _pedestrianStatusStream;
+  double? userCurrentLatitude;
+  double? userCurrentLongitude;
+  double? userMainLatitude;
+  double? userMainLongitude;
+  double? distanceTravelled;
   String _status = '0', _steps = '0';
+  final Future<SharedPreferences> preferences = SharedPreferences.getInstance();
+  // StreamController<double> controller = StreamController<double>();
 
   @override
   void initState() {
-    super.initState();
     initPlatformState();
+    getUserLocationDetails();
+    super.initState();
+  }
+
+  void getUserLocationDetails() async {
+    final SharedPreferences prefs = await preferences;
+    userMainLatitude = double.parse(prefs.getString('userLatitude') ?? "0");
+    userMainLongitude = double.parse(prefs.getString('userLongitude') ?? "0");
+
+    userCurrentLatitude =
+        double.parse(prefs.getString('userCurrentLatitude') ?? "0");
+    userCurrentLongitude =
+        double.parse(prefs.getString('userCurrentLongitude') ?? "0");
+
+    distanceTravelled =
+        double.parse(prefs.getString('distanceTravelled') ?? "0");
   }
 
   void onStepCount(StepCount event) {
@@ -62,52 +87,75 @@ class _PedometerScreenState extends State<PedometerScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // controller.stream.listen((value) {
+    //   print('Value from controller: $value');
+    // });
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('FITPATH'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'Steps taken:',
-                style: TextStyle(fontSize: 30),
-              ),
-              Text(
-                _steps,
-                style: const TextStyle(fontSize: 60),
-              ),
-              const Divider(
-                height: 100,
-                thickness: 0,
-                color: Colors.white,
-              ),
-              const Text(
-                'Pedestrian status:',
-                style: TextStyle(fontSize: 30),
-              ),
-              Icon(
-                _status == 'walking'
-                    ? Icons.directions_walk
-                    : _status == 'stopped'
-                        ? Icons.accessibility_new
-                        : Icons.error,
-                size: 100,
-              ),
-              Center(
-                child: Text(
-                  _status,
-                  style: _status == 'walking' || _status == 'stopped'
-                      ? const TextStyle(fontSize: 30)
-                      : const TextStyle(fontSize: 20, color: Colors.red),
-                ),
-              )
-            ],
+          appBar: AppBar(
+            title: const Text('FITPATH'),
           ),
-        ),
-      ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Distance Travelled: ${distanceTravelled ?? ""}',
+                  style: const TextStyle(fontSize: 20),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  'Initial Latitude: ${userMainLatitude ?? ""}, \nInitial Longitude : ${userMainLongitude ?? ""}',
+                  style: const TextStyle(fontSize: 20),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  'Current Latitude: ${userCurrentLatitude ?? ""}, \nCurrent Longitude : ${userCurrentLongitude ?? ""}',
+                  style: const TextStyle(fontSize: 20),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Text(
+                  'Steps taken:',
+                  style: TextStyle(fontSize: 30),
+                ),
+                Text(
+                  _steps,
+                  style: const TextStyle(fontSize: 60),
+                ),
+                const Divider(
+                  height: 100,
+                  thickness: 0,
+                  color: Colors.white,
+                ),
+                const Text(
+                  'Pedestrian status:',
+                  style: TextStyle(fontSize: 30),
+                ),
+                Icon(
+                  _status == 'walking'
+                      ? Icons.directions_walk
+                      : _status == 'stopped'
+                          ? Icons.accessibility_new
+                          : Icons.error,
+                  size: 100,
+                ),
+                Center(
+                  child: Text(
+                    _status,
+                    style: _status == 'walking' || _status == 'stopped'
+                        ? const TextStyle(fontSize: 30)
+                        : const TextStyle(fontSize: 20, color: Colors.red),
+                  ),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,17 @@ import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:flutter_background_service_android/flutter_background_service_android.dart';
+import 'package:geolocator/geolocator.dart';
 
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app/routes/app_pages.dart';
+import 'dart:math' show pi, pow, cos, sin, asin, sqrt;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await userPermission();
   await initializeService();
   runApp(
     GetMaterialApp(
@@ -22,6 +25,46 @@ Future<void> main() async {
       getPages: AppPages.routes,
     ),
   );
+}
+
+Future<bool> isFirstTime() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs = await SharedPreferences.getInstance();
+  var isFirstTime = prefs.getBool('first_time');
+  if (isFirstTime != null && !isFirstTime) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+userPermission() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var isFirstTimeVal = await isFirstTime();
+  print("isFirst Time $isFirstTimeVal");
+
+  var permission = await Geolocator.requestPermission();
+  if (permission == LocationPermission.denied ||
+      permission == LocationPermission.deniedForever) {
+    // TODO: make a dialog to open settings
+  } else if (permission == LocationPermission.always ||
+      permission == LocationPermission.whileInUse) {
+    if (isFirstTimeVal) {
+      Position myLocation = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+
+      prefs.setBool('first_time', false);
+      await prefs.setString('userLatitude', myLocation.latitude.toString());
+      await prefs.setString('userLongitude', myLocation.longitude.toString());
+    } else {
+      var userLatitude = double.parse(prefs.getString('userLatitude') ?? "");
+      var userLongitude = double.parse(prefs.getString('userLongitude') ?? "");
+
+      print({userLatitude, userLongitude});
+    }
+  } else {
+    // display info message to user saying this feature is restricted in your device
+  }
 }
 
 Future<void> initializeService() async {
@@ -56,20 +99,20 @@ bool onIosBackground(ServiceInstance service) {
 
 void onStart(ServiceInstance service) async {
   // Only available for flutter 3.0.0 and later
+
   DartPluginRegistrant.ensureInitialized();
 
   // For flutter prior to version 3.0.0
   // We have to register the plugin manually
 
   SharedPreferences preferences = await SharedPreferences.getInstance();
-  await preferences.setString("hello", "world");
 
   if (service is AndroidServiceInstance) {
     service.on('setAsForeground').listen((event) {
       service.setAsForegroundService();
     });
 
-    service.on('setAsBackground').listen((event) {
+    service.on('setAsBackground').listen((event) async {
       service.setAsBackgroundService();
     });
   }
@@ -78,19 +121,65 @@ void onStart(ServiceInstance service) async {
     service.stopSelf();
   });
 
+  var userLatitude = double.parse(preferences.getString('userLatitude') ?? "0");
+  var userLongitude =
+      double.parse(preferences.getString('userLongitude') ?? "0");
+
+  late Position currentUserLocation;
+  // StreamController<double> controller = StreamController<double>();
   // bring to foreground
   Timer.periodic(const Duration(seconds: 1), (timer) async {
     final hello = preferences.getString("hello");
 
     if (service is AndroidServiceInstance) {
-      service.setForegroundNotificationInfo(
-        title: "My App Service",
-        content: "Updated at ${DateTime.now()}",
-      );
+      // service.setForegroundNotificationInfo(
+      //   title: "My App Service",
+      //   content: "Updated at ${DateTime.now()}",
+      // );
     }
 
+    currentUserLocation = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    await preferences.setString(
+        'userCurrentLatitude', currentUserLocation.latitude.toString());
+    await preferences.setString(
+        'userCurrentLongitude', currentUserLocation.longitude.toString());
+
+    double userLatitudeRad = userLatitude * pi / 180;
+    double currentUserLatitudeRad = currentUserLocation.latitude * pi / 180;
+
+    double userLongitudeRad = userLongitude * pi / 180;
+    double currentUserLongitudeRad = currentUserLocation.longitude * pi / 180;
+
+    // Haversine formula
+    double dLongitude = currentUserLongitudeRad - userLongitudeRad;
+    double dLatitude = currentUserLatitudeRad - userLatitudeRad;
+    double a = pow(sin(dLatitude / 2), 2) +
+        cos(userLatitudeRad) *
+            cos(currentUserLatitudeRad) *
+            pow(sin(dLongitude / 2), 2);
+
+    double c = 2 * asin(sqrt(a));
+
+    // Radius of earth in kilometers. Use 3956 for miles
+    int radius = 6371;
+
+    // calculate the result in meters
+    double distance = (c * radius * 1000);
+
+    // controller.add(distance);
+
+    await preferences.setString(
+        'distanceTravelled', (distance.floor()).toString());
+
     /// you can see this log in logcat
-    print('FLUTTER BACKGROUND SERVICE: ${DateTime.now()}');
+    // print(
+    //     'FLUTTER BACKGROUND SERVICE: ${DateTime.now()} yeah ${currentUserLocation.latitude} ${currentUserLocation.longitude} hello $distance');
+    // print('$userLatitude, $userLongitude');
+    // print('${currentUserLocation.latitude}, ${currentUserLocation.longitude}');
+    print('$distance');
+
     // test using external plugin
     final deviceInfo = DeviceInfoPlugin();
     String? device;
